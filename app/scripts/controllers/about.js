faf370aac8d95918f116b0b8c7109ea66d2aa088
LAlang <langxue_dalang@163.com>
2017-08-28 15:44:06 +0800

    建立关于页面

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name homepageApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the homepageApp
+ */
+
+angular.module('homepageApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'about',
+      'hghg',
+      'Karma'
+    ];
+  });
